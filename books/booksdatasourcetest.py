5eb01e6d959d7d1ad9632853aa5c3f296acf2e13
Owen Barnett <owenbarnett@eduroam-167-48.dyn.carleton.edu>
2018-09-19 12:19:20 -0500

    Worked on test

--- books/booksdatasourcetest.py
+++ books/booksdatasourcetest.py
@@ -1,1 +1,22 @@
-#test
+#Authors: Owen Barnett and Katherine Sweeney
+
+'''
+   primecheckertests.py
+   Jeff Ondich, 9 May 2012
+   Updated for use in a lab exercise, 4 Nov 2013
+'''
+
+import booksdatasource
+import unittest
+
+class BooksDataSourceTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
