06a5c044ad7869a012b633e275e06542271069c2
Owen Barnett <owenbarnett@eduroam-167-48.dyn.carleton.edu>
2018-10-28 16:19:03 -0500

    Worked on website

--- webapp/static/planets.js
+++ webapp/static/planets.js
@@ -304,7 +304,7 @@ function display(button) {
     }
 
 
-    fetch(query, {method: 'get'}).then((response) => response.json())
+    fetch(query)
     .then(function(planet_star_list) {
         document.getElementById("results_table").innerText = planet_star_list
     })
