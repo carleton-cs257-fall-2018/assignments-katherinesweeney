66fcb3cce840535408bc238461e6edbf562e53c1
Owen Barnett <owenbarnett@eduroam-167-48.dyn.carleton.edu>
2018-10-29 11:55:46 -0500

    Worked on webstie

--- webapp/static/planets.js
+++ webapp/static/planets.js
@@ -318,21 +318,22 @@ function display(button) {
     fetch(query, {method: 'get'}).then((response) => response.json()).then(function(planet_star_list) {
         console.log(planet_star_list)
         display_features = get_display_features();
-        var tableBody = '<tr>'
+        var tableBody = '<tr>';
         for (var index = 0; index < display_features.length; index++) {
-            tableBody+='<th>'
-            tableBody+=display_features[index]
-            tableBody+="</th>"
+            tableBody+='<th>';
+            tableBody+=display_features[index];
+            tableBody+="</th>";
         }
-        tableBody+="</tr>"
+        tableBody+="</tr>";
 
         for (var index = 0; index < planet_star_list.length; index++) {
             planet_star = planet_star_list[index];
-            tableBody+="<tr>"
+            tableBody+="<tr>";
             for (var display_index = 0; display_index < display_features.length; display_index++) {
-                tableBody+="<td>"
-                tableBody+=planet_star[display_features[display_index]]
-                tableBody+="</td>"
+                // tableBody+="<td>";
+                // tableBody+=planet_star[display_features[display_index]];
+                // tableBody+="</td>";
+                console.log(planet_star[display_features[display_index]])
             }
             tableBody+="</tr>"
         }
