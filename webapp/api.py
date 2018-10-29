10deb0d995d7f42456d482ef947040437da5bb70
Owen Barnett <owenbarnett@eduroam-167-48.dyn.carleton.edu>
2018-10-29 11:12:10 -0500

    Changed API

--- webapp/api.py
+++ webapp/api.py
@@ -238,7 +238,7 @@ def get_planets():
     planets = []
     connection = get_connection()
     cursor = connection.cursor()
-    query = '''SELECT planets.*, discovery_methods.name, discovery_facility.name
+    query = '''SELECT planets.*
                FROM planets, stars, discovery_methods, discovery_facility
                WHERE planets.pl_name LIKE %s
                AND planets.pl_host_star_id = stars.st_id 
