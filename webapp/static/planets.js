7611498379c4c7bec343b65e4f366a287b426108
Owen Barnett <owenbarnett@eduroam-167-48.dyn.carleton.edu>
2018-10-30 16:25:51 -0500

    Worked on webstie

--- webapp/static/planets.js
+++ webapp/static/planets.js
@@ -296,7 +296,22 @@ function get_display_features() {
 
 }
 
-async function get_planet_name(value) {
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+function get_planet_name(value) {
     if (value == "None") {
         return "None"
     }
