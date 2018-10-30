0865ea0d8fb28d9903346b3d542b3e7945eb6649
Owen Barnett <owenbarnett@eduroam-167-48.dyn.carleton.edu>
2018-10-30 15:26:26 -0500

    Worked on webstie

--- webapp/static/planets.js
+++ webapp/static/planets.js
@@ -302,14 +302,13 @@ function get_planet_name(value) {
     }
     query = getBaseURL() + "/planet/" + value;
     console.log(query)
-    fetch(query, {method: 'get'}).then((response) => response.json()).then(function(planet_list) {
+    return fetch(query, {method: 'get'}).then((response) => response.json()).then(function(planet_list) {
         console.log(planet_list);
         var planet = planet_list[0];
         var planet_name = planet['Planet Name'];
         console.log(planet);
         console.log(planet_name)
     })
-    return planet_name;
 }
 
 function display(button) {
