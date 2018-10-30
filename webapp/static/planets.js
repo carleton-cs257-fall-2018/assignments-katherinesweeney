8b0921885a54c2ab85a6bdf8b977caa1dd33e4ea
Owen Barnett <owenbarnett@eduroam-167-48.dyn.carleton.edu>
2018-10-30 16:02:13 -0500

    Worked on webstie

--- webapp/static/planets.js
+++ webapp/static/planets.js
@@ -308,7 +308,7 @@ function get_planet_name(value) {
         console.log(planet_name)
         window.planet_name = planet_name;
         console.log(window.planet_name)
-    }).then(console.log(window.planet_name))
+    }).finally("test" + console.log(window.planet_name))
     console.log(window.planet_name)
     // console.log("break")
     // console.log(window.planet_name)
