2283a16bf371919d23830afd74db56d41b2f0b9c
Owen Barnett <owenbarnett@eduroam-167-48.dyn.carleton.edu>
2018-11-10 13:59:27 -0600

    Wrote class headers

--- final/src/boxing/Boxer.java
+++ final/src/boxing/Boxer.java
@@ -4,15 +4,26 @@ package boxing;
 public class Boxer {
 
     /**
+     * Initializes a new boxer
      *
-     * @param isRight
-     * @param start_position
-     * @param width
+     * @param isRight boxer is the right boxer on screen
+     * @param start_position position boxer starts at
+     * @param width width of the boxer on the screen
      */
     public boxer(boolean isRight, int start_position, int width) { }
 
+    /**
+     * Sets the opponent Boxer reference
+     *
+     * @param opponent Oponent Boxer object
+     */
     public void addOpponent(Boxer opponent) { }
 
+    /**
+     * 
+     *
+     * @param moveRight
+     */
     public void move(boolean moveRight) { }
 
     public int getPosition() { return 1 }
