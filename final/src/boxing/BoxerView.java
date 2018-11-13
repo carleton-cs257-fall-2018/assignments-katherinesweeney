55d145edffb8b2a456b33069d8c97400ae1e04b3
HahnJustin <hahnj2@gmail.com>
2018-11-13 16:23:30 -0600

    added some functions

--- final/src/boxing/BoxerView.java
+++ final/src/boxing/BoxerView.java
@@ -1,5 +1,14 @@
 package boxing;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 /**
  * BoxerView.java
  * A View class that sets up the user's view
@@ -7,6 +16,10 @@ package boxing;
  * @author Owen Barnett, Justin Hahn, Kate Sweeney
  */
 public class BoxerView{
+    public final static double CELL_WIDTH = 5.0;
+    public final static double CELL_HEIGHT = 75.0;
+    @FXML private int cellCount;
+    private Rectangle[] cellViews;
 
     /**
      * Initializes the BoxerView
@@ -29,12 +42,29 @@ public class BoxerView{
     /**
      * Gets width of fight screen
      */
-    public void getWidth() { }
+    public int getWidth() { return this.cellCount; }
 
+    public void setWidth(int cellCount) {
+        this.cellCount = cellCount;
+        this.initializeGame();
+    }
     /**
      * Initializes game screen for user to character select screen
      */
-    private void initializeGame() { }
+    private void initializeGame() {
+        if (this.cellCount > 0) {
+            this.cellViews = new Rectangle[this.cellCount];
+            for (int row = 0; row < this.cellCount; row++) {
+                Rectangle rectangle = new Rectangle();
+                rectangle.setX(CELL_HEIGHT/2); // FIX MAYBE TODO FIX ME
+                rectangle.setY((double)row * CELL_WIDTH);
+                rectangle.setWidth(CELL_WIDTH);
+                rectangle.setHeight(CELL_HEIGHT);
+                this.cellViews[row]= rectangle;
+                this.getChildren().add(rectangle);
+            }
+        }
+    }
 
     /**
      * Updates user view according to actions of Boxers
